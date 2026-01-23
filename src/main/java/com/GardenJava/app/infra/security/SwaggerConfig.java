8e82766bbba6e2b89912e91228f2acbe5688bb5f
victorpizzarro <victor.pizzarrorj@gmail.com>
2026-01-23 00:34:30 -0300

    feat(infra): configuração inicial do swagger

---
+++ src/main/java/com/GardenJava/app/infra/security/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.GardenJava.app.infra.security;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Garden Java API",
+                version = "1.0",
+                description = "API com autenticação JWT"
+        ),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+public class SwaggerConfig {
+}
