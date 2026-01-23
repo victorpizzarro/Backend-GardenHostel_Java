7bbe58f0691c0af20c2516c5b49bab5f8a763f3e
victorpizzarro <victor.pizzarrorj@gmail.com>
2026-01-22 22:08:50 -0300

    chore: estrutura inicial do projeto

---
+++ src/main/java/com/GardenJava/app/GardenHostelJavaApplication.java
@@ -0,0 +1,13 @@
+package com.GardenJava.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenHostelJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardenHostelJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GardenJava/app/GardenHostelJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GardenJava.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardenHostelJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
